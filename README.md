87a2117d38552045dab9274eb2eb3b16cae0a3b4
alvaarogalvez <aalvarogf@gmail.com>
2025-03-11 18:22:00 +0100

    build

---
+++ README.md
@@ -0,0 +1,17 @@
+1. creamos el repo local
+
+```bash
+git init
+git add .
+git commit -m "build"
+
+```
+subimos el repo a github
+
+```bash
+gh repo create
+gh repo list
+``````
+
+3. creamos una app para desplegar
+
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("hola mundo");
+
+});
+
+app.listen(3000, () => {
+    console.log("server is runnning on port 3000");
+
+
+});
